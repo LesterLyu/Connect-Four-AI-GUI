c4a0c923ac3ece6abd862532b5b3e67976c44abc
jonathonyan <jonathon_yan1997@hotmail.com>
2017-08-09 16:38:47 -0400

    AI vs AI gui bugs fixed (almost finished)

--- gui.py
+++ gui.py
@@ -9,6 +9,7 @@ EXIT_BUTTON2 = "Exit2"
 RESTART_BUTTON = "Restart"
 START_BUTTON = "Start"
 START_BUTTON2 = "Start2"
+NEXT_BUTTON = "Next"
 
 
 class GUI:
@@ -16,19 +17,17 @@ class GUI:
         self.app = None
         self.game = game
         self.waiting = 0
-        self.ai_vs_ai = 0
 
     # handle button events
     def press(self, button):
         if button == EXIT_BUTTON or button == EXIT_BUTTON2:
             self.app.stop()
         elif button == START_BUTTON and self.waiting == 0:
-
             self.game.reset()
             self.update()
-
+            self.game.change_mode(3)
             # play the first move
-            self.game.is_running = True
+            # self.game.is_running = True
 
             if self.app.getRadioButton("diff") == "Normal":
                 print("set normal")
@@ -59,15 +58,53 @@ class GUI:
             print("AI vs AI")
             self.game.reset()
             self.update()
-            self.game.is_running = True
+            self.game.change_mode(4)
+            self.game.p1 = Constants.AI_1_NAME
+            self.game.p2 = Constants.AI_2_NAME
+            self.game.curr_turn = self.game.p1
 
-        elif self.waiting == 0 and self.game.is_running == 1 and self.ai_vs_ai == 0 and button[-8:] != "AI vs AI":
-            print("pressed button is " + button[-8:] + " " + str(button[-8:] == "AI vs AI"))
+
+            self.game.is_running = 1
+            if self.app.getRadioButton("diff1") == "Normal":
+                print("set AI 1 normal")
+                self.game.difficulty = Constants.NORMAL
+            elif self.app.getRadioButton("diff1") == "Hard":
+                print("set AI 1 hard")
+                self.game.difficulty = Constants.HARD
+            else:
+                print("set AI 1 easy")
+                self.game.difficulty = Constants.EASY
+
+            if self.app.getRadioButton("diff2") == "Normal":
+                print("set AI 2 normal")
+                self.game.difficulty2 = Constants.NORMAL
+            elif self.app.getRadioButton("diff2") == "Hard":
+                print("set AI 2 hard")
+                self.game.difficulty2 = Constants.HARD
+            else:
+                print("set AI 2 easy")
+                self.game.difficulty2 = Constants.EASY
+            self.app.setButton(START_BUTTON2, RESTART_BUTTON)
+            t3 = Thread(target=self.game.play, args=(self, self.game.curr_turn))
+            t3.start()
+            self.game.curr_turn = self.game.p2
+
+
+        elif button == NEXT_BUTTON and self.waiting == 0 and self.game.is_running == 1 and self.game.mode == 4:
+            t4 = Thread(target=self.game.play, args=(self, self.game.curr_turn,))
+            t4.start()
+            if self.game.curr_turn == self.game.p1:
+                self.game.curr_turn = self.game.p2
+            else:
+                self.game.curr_turn = self.game.p1
+
+        elif self.waiting == 0 and self.game.is_running == 1 and self.game.mode == 3 and button[-6:] == "Single":
+            print("pressed button is " + button[-8:] + " " + str(button[-6:] == "Single"))
             col = int(button[1])
             thread = Thread(target=self.game.play, args=(self, col,))
             thread.start()
         else:
-            print(str(button[-8:] == "AI vs AI"))
+            # print(str(button[-8:] == "AI vs AI"))
             print(self.game.is_running, self.waiting)
 
     def reset(self):
@@ -79,14 +116,17 @@ class GUI:
     def update(self):
         for row in range(self.game.num_rows):
             for col in range(self.game.num_cols):
-                self.app.setButton(str(row) + str(col), self.game.board[row][col])
+                self.app.setButton(str(row) + str(col) + "Single", self.game.board[row][col])
+        for row in range(self.game.num_rows):
+            for col in range(self.game.num_cols):
+                self.app.setButton(str(row) + str(col) + "AI vs AI", self.game.board[row][col])
 
     def init_window(self):
         # create a GUI variable called app
         self.app = gui("Connect Four", "800x550")
         #app.setBg("orange")
         self.app.startTabbedFrame("TabbedFrame")
-        self.app.startTab("Tab1")
+        self.app.startTab("Player vs AI")
         self.app.addLabel("l1", "Player vs AI")
 
         self.app.setExpand("both")
@@ -111,7 +151,7 @@ class GUI:
 
         for row in range(self.game.num_rows):
             for col in range(self.game.num_cols):
-                self.app.addNamedButton(" ", str(row) + str(col), self.press, row, col)
+                self.app.addNamedButton(" ", str(row) + str(col) + "Single", self.press, row, col)
 
         self.app.stopFrame()
 
@@ -124,7 +164,7 @@ class GUI:
         self.app.stopFrame()
         self.app.stopTab()
 
-        self.app.startTab("Tab2")
+        self.app.startTab("AI vs AI")
         self.app.addLabel("l2", "AI vs AI")
         self.app.setExpand("both")
         self.app.setPadding([10, 10])
@@ -156,8 +196,10 @@ class GUI:
         self.app.setSticky("")
         # link the buttons to the function called press
         self.app.addNamedButton(START_BUTTON, START_BUTTON2, self.press, row=0, column=0)
+        self.app.addNamedButton(NEXT_BUTTON, NEXT_BUTTON, self.press, row=0, column=1)
+
         # self.app.addButton(RESTART_BUTTON, self.press, row=0, column=1)
-        self.app.addNamedButton(EXIT_BUTTON, EXIT_BUTTON2, self.press, row=0, column=1)
+        self.app.addNamedButton(EXIT_BUTTON, EXIT_BUTTON2, self.press, row=0, column=2)
         self.app.stopFrame()
         self.app.stopTab()
         self.app.stopTabbedFrame()
