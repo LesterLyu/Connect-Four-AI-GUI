e23c25ac0a313c65ccfffa79a931b718f84b310c
jonathonyan <jonathon_yan1997@hotmail.com>
2017-08-14 16:29:35 -0400

    add test ai vs ai

--- test_ai_vs_ai.py
+++ test_ai_vs_ai.py
@@ -2,6 +2,8 @@ from connect_four import *
 
 
 if __name__ == "__main__":
+    print("Test AI vs AI with the same difficulty")
+    print("The AI {} will always go first".format(Constants.AI_1_NAME))
     for d in range(1, 7):
         print("-----------------------------------------------------")
         print("testing depth {} ......".format(d))
